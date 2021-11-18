ca148b551c6dade4a137db0e96f888130112c2b0
Seb <seb@peiris.ch>
2021-11-18 11:25:51 +0100

    rail de c

--- DataMgmt/lab3/work/index.py
+++ DataMgmt/lab3/work/index.py
@@ -1,3 +1,4 @@
+from os import name
 from neo4j import GraphDatabase
 import pandas as pd
 
@@ -22,6 +23,20 @@ class GenerateTrainNetwork:
                     row['population']
                 )
 
+    def create_lines(self):
+        lines = pd.read_csv('data/lines.csv', sep=';')
+        for index, row in lines.iterrows():
+            with self.driver.session() as session:
+                session.write_transaction(
+                    self._create_line,
+                    row['city1'],
+                    row['city2'],
+                    row['km'],
+                    row['time'],
+                    row['nbTracks']
+                )
+
+
     @staticmethod
     def _create_city(tx, name, latitude, longitude, population):
         query = (
@@ -35,9 +50,26 @@ class GenerateTrainNetwork:
         city_created = result.single()['c']
         print("Created City: {name}".format(name=city_created['name']))
 
+    @staticmethod
+    def _create_line(tx, city1, city2, km, time, nbTracks):
+        query = (
+            """
+            MATCH (c1:City), (c2:City)
+            WHERE c1.name = $city1 AND c2.name = $city2
+            CREATE (c1)-[l1:Line {km: $km, time: $time, nbTracks: $nbTracks}]->(c2)
+            CREATE (c2)-[l2:Line {km: $km, time: $time, nbTracks: $nbTracks}]->(c1)
+            RETURN l1, l2
+            """
+        )
+        result = tx.run(query, city1=city1, city2=city2, km=km, time=time, nbTracks=nbTracks)
+
+        line_created = result.single()['l1']
+        print("Created Line: {city1} - {city2}".format(city1=line_created['c1'], city2=line_created['c2']))
+
 
 if __name__ == "__main__":
     generate_train_network = GenerateTrainNetwork("neo4j://localhost:7687")
 
     # create all city nodes
     generate_train_network.create_cities()
+    generate_train_network.create_lines()
